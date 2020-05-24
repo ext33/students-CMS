7efa3136ea9ce9a01e851b771593a8a4af84c41d
Egor <redwoodx3@gmail.com>
2020-05-25 00:00:09 +0300

    add models and admin panel

--- CMS/CMSWeb/admin.py
+++ CMS/CMSWeb/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+@admin.register(Users)
+class Users(admin.ModelAdmin):
+    list_display = ('FIO', 'group', 'telephone')
+    list_filter = ('FIO', 'group')
+
+
+@admin.register(Groups)
+class Groups(admin.ModelAdmin):
+    list_display = ('group', 'direction')
+    list_filter = ('course', 'direction')
+
+
+@admin.register(Subject)
+class Subjects(admin.ModelAdmin):
+    list_display = ('subject', 'teacher')
+    list_filter = ('teacher', 'course')
+
+
+@admin.register(Performance)
+class Perf(admin.ModelAdmin):
+    list_display = ('FIO', 'subject', 'mark', 'date')
+    list_filter = ('FIO', 'subject', 'mark', 'date', 'reporting_form')
--- CMS/CMSWeb/models.py
+++ CMS/CMSWeb/models.py
@@ -1,3 +1,123 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Groups(models.Model):
+    group = models.CharField(
+        max_length=200,
+        verbose_name='Группа'
+    )
+    course = models.IntegerField(
+        null=True,
+        verbose_name='Курс'
+    )
+    direction = models.CharField(
+        null=True,
+        max_length=200,
+        verbose_name='Специальность'
+    )
+
+    class Meta:
+        verbose_name = 'Группы'
+        verbose_name_plural = 'Группы'
+
+    def __str__(self):
+        return self.group
+
+
+class Subject(models.Model):
+    subject = models.CharField(
+        max_length=300,
+        verbose_name='Предмет'
+    )
+    teacher = models.CharField(
+        max_length=200,
+        verbose_name='Преподаватель'
+    )
+    course = models.IntegerField(
+        verbose_name='Курс'
+    )
+
+    class Meta:
+        verbose_name = 'Предметы'
+        verbose_name_plural = 'Предметы'
+
+    def __str__(self):
+        return self.subject
+
+
+class Users(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.DO_NOTHING,
+        verbose_name='Пользователь'
+    ),
+    email = models.EmailField(
+        max_length=200,
+        verbose_name='Почта'
+    )
+    telephone = models.CharField(
+        max_length=12,
+        verbose_name='Телефон'
+    )
+    group = models.ForeignKey(
+        Groups,
+        on_delete=models.DO_NOTHING,
+        verbose_name='Группа'
+    )
+    FIO = models.CharField(
+        max_length=200,
+        verbose_name='ФИО'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователи'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.FIO
+
+
+class Performance(models.Model):
+    FORMS = (
+        ('exam', 'Экзамен'),
+        ('offset', 'Зачет')
+    )
+
+    FIO = models.ForeignKey(
+        Users,
+        on_delete=models.DO_NOTHING,
+        verbose_name='ФИО студента'
+    )
+    group = models.ForeignKey(
+        Groups,
+        on_delete=models.DO_NOTHING,
+        verbose_name='Группа'
+    )
+    subject = models.ForeignKey(
+        Subject,
+        on_delete=models.DO_NOTHING,
+        verbose_name='Предмет'
+    )
+    mark = models.CharField(
+        max_length=3,
+        verbose_name='Оценка'
+    )
+    reporting_form = models.CharField(
+        max_length=20,
+        choices=FORMS,
+        verbose_name='Форма отчетности'
+    )
+    date = models.DateField(
+        null=True,
+        verbose_name='Дата'
+    )
+
+    class Meta:
+        verbose_name = 'Успеваемость'
+        verbose_name_plural = 'Успеваемость'
+
+    def __str__(self):
+        return self.FIO
