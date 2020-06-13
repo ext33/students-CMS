519682570dd0a6788cfe3bc1e5cb9a6ee72a4200
Egor <redwoodx3@gmail.com>
2020-06-13 15:11:15 +0300

    upd front and docker config

--- CMS/entrypoint.sh
+++ CMS/entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py createsuperuser --no-input
-gunicorn --bind=0.0.0.0:8000 CMS.wsgi:application
+python manage.py runserver 0.0.0.0:8000
--- dockerfile
+++ dockerfile
@@ -1,6 +1,8 @@
 FROM python:3.8
 
-COPY ./CMS /srv/www/CMS
+COPY ./CMS/req.txt /srv/www/CMS/
+COPY ./CMS/entrypoint.sh /srv/www/CMS/
 WORKDIR /srv/www/CMS
 
+RUN chmod +x entrypoint.sh
 RUN pip install -r req.txt
