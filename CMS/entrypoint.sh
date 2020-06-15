38ade58356564d3233927ce2bd273aa8f2d68b5a
Egor <redwoodx3@gmail.com>
2020-06-15 17:30:29 +0300

    upd django version & nginx config

--- CMS/entrypoint.sh
+++ CMS/entrypoint.sh
@@ -2,3 +2,4 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py createsuperuser --no-input
 python manage.py runserver 0.0.0.0:8000
+# gunicorn --workers=2 --bind=0.0.0.0:8000 CMS.wsgi:application
